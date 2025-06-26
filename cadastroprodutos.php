4e28beeadd65b999f49fdc0bdfc18b4cb7a959d0
Vitor Hugo de Lima Boeira <boeiravitorhugo@gmail.com>
2025-06-26 07:42:55 -0300

    Add files via upload

--- cadastroprodutos.php
+++ cadastroprodutos.php
@@ -1,48 +1,72 @@
 <?php
 
-echo "\t\t\033[4m\033[1mMENU\n\n1 - ADICIONAR PRODUTO\n2 - BUSCAR PRODUTO\nsair - SAIR\n\033[0m\033[0m\n";
+echo "\t\t\033[4m\033[1mMENU\n\n1 - ADICIONAR PRODUTO\n2 - BUSCAR E ALTERAR PRODUTO\nsair - SAIR\n\033[0m\033[0m\n";
 
-do{
-$terminal = readline("Selecione uma opção: ");
+do {
+    $terminal = readline("Selecione uma opção: ");
 
-switch($terminal){
-    
-    case 1:
-        $produto = readline("Digite o nome do produto: ");
-        $preço = readline("Digite o valor do produto: ");
-        $produtos = [
-            'produto' => $produto,
-            'preço' => $preço
-        ];
-       $conteudo = serialize($produtos);
-        if (file_put_contents("buscaealteraçãodedados.txt", $conteudo . "\n")!== false){
-            echo "produto adicionado com sucesso!\n";
-        }
-        else {
-            echo "falha ao tentar adicionar o produto!";
-        }
-        break;
-    case 2:
-        $conteudo = file_get_contents("buscaealteraçãodedados.txt");
-        $produtos = unserialize($conteudo);
-        $buscar = readline("Digite o produto que deseja alterar o valor: ");
-        if (array_key_exists($buscar, $produtos)){
-            $novopreco = readline("Digite o novo preço: ");
-            $buscar[$preço] = $novopreco;
-        }
-        else{
-            echo "esse produto não existe!";
-        }
-        break;
-    case "sair":
-        echo "SAINDO";
-        break;
-    
-    default:
-        echo "esse comando nao existe tente novamente!\n";
-        break;
-}
+    switch ($terminal) {
+
+        case 1:
+            $produto = readline("Digite o nome do produto: ");
+            $preco = readline("Digite o valor do produto: ");
+
+            if (file_exists("buscaealteraçãodedados.txt")) {
+                $produtos = unserialize(file_get_contents("buscaealteraçãodedados.txt"));
+            } else {
+                $produtos = [];
+            }
+
+            $produtos[] = [
+                'produto' => $produto,
+                'preco' => $preco
+            ];
+
+            if (file_put_contents("buscaealteraçãodedados.txt", serialize($produtos)) !== false) {
+                echo "Produto adicionado com sucesso!\n";
+            } else {
+                echo "Falha ao tentar adicionar o produto!\n";
+            }
+            break;
+
+        case 2:
+            if (!file_exists("buscaealteraçãodedados.txt")) {
+                echo "Nenhum produto cadastrado ainda!\n";
+                break;
+            }
+
+            $produtos = unserialize(file_get_contents("buscaealteraçãodedados.txt"));
+            $buscar = readline("Digite o nome do produto que deseja alterar o preço: ");
+
+            $produtoencontrado = false;
+
+            foreach ($produtos as &$item) {
+                    $novoPreco = readline("Digite o novo preço: ");
+                    $item['preco'] = $novoPreco;
+                    $produtoencontrado = true;
+                    break;
+                }
+
+            if ($produtoencontrado) {
+                if (file_put_contents("buscaealteraçãodedados.txt", serialize($produtos)) !== false) {
+                    echo "Preço alterado com sucesso!\n";
+                }   else {
+                    echo "Falha ao tentar salvar as alterações!\n";
+                    }
+            } else {
+                echo "Esse produto não existe!\n";
+              }
+            break;
+
+        case "sair":
+            echo "SAINDO...\n";
+            break;
+
+        default:
+            echo "Esse comando não existe, tente novamente!\n";
+            break;
     }
-while($terminal !== "sair");
+}
+while ($terminal !== "sair");
 
 ?>
